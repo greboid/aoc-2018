152828178abec434d92e383ea186a125d7e20067
Greg Holmes <git@greg.holmes.name>
2018-12-02 19:56:35 +0000

    Might as well not carry on once we've found one.

--- 2/b.php
+++ 2/b.php
@@ -5,8 +5,8 @@ foreach ($file as $a) {
   foreach ($file as $b) {
     $diff = levenshtein($a, $b);
     if ($diff ==1) {
-      $results[] = implode(array_intersect_assoc(str_split($a), str_split($b)))."\r\n";
+      echo implode(array_intersect_assoc(str_split($a), str_split($b)))."\r\n";
+      break 2;
     }
   }
 }
-echo array_unique($results)[0];
