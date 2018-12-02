1348d4c8197b2548666fef1565bfdd56c97cc8c8
Greg Holmes <git@greg.holmes.name>
2018-12-02 23:38:39 +0000

    Optimise day 1.

--- 1/run.php
+++ 1/run.php
@@ -1,24 +1,18 @@
 <?php
-$file_handle = fopen(__DIR__."/input.txt", "r");
-$value = 0;
-while (!feof($file_handle)) {
-   $value += intval(fgets($file_handle));
-}
-echo "Part 1: ".$value."\r\n";
-
-$file = new SplFileObject(__DIR__."/input.txt");
+$lines = file(__DIR__."/input.txt");
 $found = false;
 $value = 0;
 $seen = array();
 $iteration = 0;
 while (!$found) {
-  while (!$file->eof() && !empty($file->current())) {
-      $value += intval($file->current());
-      $found = array_key_exists(strval($value), $seen);
-      if ($found) { echo "Part 2: " .$value ." (iterations: ".$iteration.")\r\n"; break 2; }
-      $seen[strval($value)] = 1;
-      $file->next();
+  foreach ($lines as $line) {
+    $value += intval($line);
+    $found = array_key_exists(strval($value), $seen);
+    if ($found) { echo "Part 2: " .$value ." (iterations: ".$iteration.")\r\n"; break 2; }
+    $seen[strval($value)] = 1;
   }
-  $file->rewind();
   $iteration++;
+  if ($iteration == 1) {
+    echo "Part 1: ".$value."\n";
+  }
 }
