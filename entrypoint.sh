25a18c47e600f44a30b3912f499636bdb0f4d8b7
Greg Holmes <git@greg.holmes.name>
2018-12-02 22:20:45 +0000

    Remove the superflous exit 1, again.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,6 @@ if [ $# -ge 1 ]; then
 fi
 if [ $# -eq 1 ]; then
   time sh -c "php $1/a.php; php $1/b.php"
-  exit 1
 elif [ $# -eq 2 ]; then
   if [ -f "/app/$1/$2.php" ]; then
     time php $1/$2.php
