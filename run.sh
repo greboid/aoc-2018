6a1a868611079fc4d993567c5d445ebacd8fb206
Greg Holmes <git@greg.holmes.name>
2018-12-02 21:39:08 +0000

    Build docker image if it doesn't exist.

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 IMAGE=greboid/aoc-2018-01
 docker image inspect $IMAGE >/dev/null 2>&1
-if [ $? -ne 0 ]
+if [ "$(docker images -q $IMAGE)" == "" ]
 then
-    echo "One time setup: building docker image..."
     docker build . -t $IMAGE
 fi
 docker run --rm -it -v $(pwd):/app $IMAGE $@
