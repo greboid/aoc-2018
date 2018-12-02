b4b75f863c098584a4b01a5b033d844158736e58
Greg Holmes <git@greg.holmes.name>
2018-12-02 11:43:12 +0000

    Day 2, part 1

---
+++ 2/a.php
@@ -0,0 +1,17 @@
+<?php
+$file = new SplFileObject(__DIR__."/input.txt");
+$twos = array();
+$threes = array();
+while (!$file->eof() && !empty($file->current())) {
+  $counts = array_count_values(str_split($file->current()));
+  foreach ($counts as $letter => $count) {
+    if ($count == 2) {
+      $twos[$file->current()] = 1;
+    }
+    if ($count == 3) {
+      $threes[$file->current()] = 1;
+    }
+  }
+  $file->next();
+}
+echo count($twos) * count($threes);
--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ if [ $# -ne 2 ]; then
     echo "Must include day and part to run"
     exit 1
 fi
-docker run --rm -v $(pwd):/app composer/composer install
-docker run --rm -it -v $PWD/$1:/data -w /data php:7.3-rc-cli php $2.php
+#docker run --rm -v $(pwd):/app composer/composer install
+docker run --rm -it -v $PWD:/app -w /app php:7.3-rc-cli php $1/$2.php
