36784a55a8623b730c5cbdd88dd5021880029bbe
Greg Holmes <git@greg.holmes.name>
2018-12-03 22:59:42 +0000

    Add ability to get answers

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+ANSWERS=0
+if [ "$1" == "--answers" ]; then
+  ANSWERS=1
+  shift
+fi
 if [ $# -ge 1 ]; then
   if [ ! -d "/app/$1" ]; then
     echo "Invalid Day"
@@ -6,7 +11,11 @@ if [ $# -ge 1 ]; then
   fi
 fi
 if [ $# -eq 1 ]; then
-  time php $1/run.php
+  if [ $ANSWERS -eq 1 ]; then
+    cat $1/answers.txt
+  else
+    time php $1/run.php
+  fi
 else
   echo "You must specify the day"
   exit 1
--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-IMAGE=greboid/aoc-2018-05
-if [ "$(docker images -q $IMAGE)" == "" ]
-then
+IMAGE=greboid/aoc-2018-06
+if [ "$(docker images -q $IMAGE)" == "" ]; then
     docker build . -t $IMAGE
 fi
 docker run --rm -it -v $(pwd):/app $IMAGE $@
