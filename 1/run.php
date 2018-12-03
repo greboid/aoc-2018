33e6acb445aedb40c6823f023eb11700d23533f5
Greg Holmes <git@greg.holmes.name>
2018-12-03 23:20:02 +0000

    Remove iterations output from day 2.

--- 1/run.php
+++ 1/run.php
@@ -8,7 +8,7 @@ while (!$found) {
   foreach ($lines as $line) {
     $value += intval($line);
     $found = array_key_exists(strval($value), $seen);
-    if ($found) { echo "Part 2: " .$value ." (iterations: ".$iteration.")\r\n"; break 2; }
+    if ($found) { echo "Part 2: " .$value ."\n"; break 2; }
     $seen[strval($value)] = 1;
   }
   $iteration++;
