5c9cd5350b0fd5c860f4e3bb7ef5a4eba25f80d0
Greg Holmes <git@greg.holmes.name>
2018-12-04 00:07:07 +0000

    Optimise day 3 a little.

--- 3/run.php
+++ 3/run.php
@@ -10,9 +10,9 @@ $claimIDs = array();
 foreach ($file as $line) {
   preg_match("/#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)/",$line,$matches);
   $claims[$matches[1]] = ['x' => $matches[2], 'y' => $matches[3], 'w' => $matches[4], 'h' => $matches[5]];
+  $claimIDs[$matches[1]] = $matches[1];
 }
 unset($file);
-$claimIDs = array_keys($claims);
 foreach ($claims as $id => $claim) {
   for($i = $claim['x']; $i < ($claim['x'] + $claim['w']); $i++) {
     for($j = $claim['y']; $j < ($claim['y'] + $claim['h']); $j++) {
@@ -22,8 +22,7 @@ foreach ($claims as $id => $claim) {
       $grid[$i][$j][] = $id;
       if (count($grid[$i][$j]) > 1) {
         foreach ($grid[$i][$j] as $value) {
-          $key = array_search($value, $claimIDs);
-          unset($claimIDs[$key]);
+          unset($claimIDs[$value]);
         }
       }
     }
