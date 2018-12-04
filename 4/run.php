ce7cee093ac26f4c23ca3fc0b21c00e22018edcf
Greg Holmes <git@greg.holmes.name>
2018-12-04 21:12:02 +0000

    Day 4.

---
+++ 4/run.php
@@ -0,0 +1,43 @@
+<?php
+
+$file = file(__DIR__."/input.txt",FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+sort($file);
+$actions = array();
+foreach ($file as $line) {
+  #[1518-03-13 23:56] Guard #2539 begins shift
+  preg_match("/\[[0-9]+-[0-9]+-[0-9]+ [0-9]+:([0-9]+)\] (.*)/",$line,$matches);
+  $actions[] = ['minute'=>$matches[1], 'action'=>$matches[2]];
+}
+$guard = 0;
+$sleepcount = array();
+$sleep = 0;
+$mostsleep = 0;
+$sleepiest = [0,0,0];
+$sleepminutes = array();
+$sleepfrequency = array();
+foreach ($actions as $action) {
+  if (preg_match('/.*#([0-9]+).*/', $action['action'], $matches)) {
+    $guard = $matches[1];
+    if (!isset($sleepcount[$guard])) {
+      $sleepcount[$guard] = 0;
+    }
+    if (!isset($sleepminutes[$guard])) {
+      $sleepminutes[$guard] = array();
+    }
+  } else if ($action['action'] == "falls asleep") {
+    $sleep = $action['minute'];
+  } else {
+    $sleepcount[$guard] += $action['minute'] - $sleep;
+    if ($sleepcount[$guard] > $mostsleep) {
+      $sleepiest = [$guard,$sleepcount[$guard], $action['minute']];
+      $mostsleep = $sleepcount[$guard];
+    }
+    for ($i = $sleep; $i < $action['minute']; $i++) {
+      if (!isset($sleepminutes[$guard][$i])) {
+        $sleepminutes[$guard][$i] = 0;
+      }
+      $sleepminutes[$guard][$i]++;
+    }
+  }
+}
+echo 'Part 1: '.(array_keys($sleepminutes[$sleepiest[0]], max($sleepminutes[$sleepiest[0]]))[0] * $sleepiest[0])."\n";
