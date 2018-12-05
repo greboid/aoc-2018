29ed6820adce2570a886e69a676104c277d82ab7
Greg Holmes <git@greg.holmes.name>
2018-12-05 21:01:41 +0000

    Optmise day 5.

--- 5/run.php
+++ 5/run.php
@@ -1,36 +1,29 @@
 <?php
 $file = file(__DIR__."/input.txt",FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 $file = $file[0];
-function react(&$input) {
-  $removed = 0;
-  for ($i = 0; $i < strlen($input) -1;) {
-    $char = $input{$i};
-    $next = $input{$i+1};
-    if (((ctype_upper($char) && ctype_lower($next)) && strtolower($char)==$next) || ((ctype_lower($char) && ctype_upper($next)) && strtoupper($char)==$next)) {
-      $input{$i}=' ';
-      $input{$i+1}=' ';
-      $removed++;
-    }
-    $i++;
-  }
-  $input = str_replace(' ','',$input);
-  return $removed;
+
+$pairs = array();
+$letters = array_keys(count_chars(strtolower($file), 1));
+foreach ($letters as $letter) {
+  $pairs[] = chr($letter).chr($letter-32);
+  $pairs[] = chr($letter-32).chr($letter);
 }
-function fullyReact(&$input) {
+
+function react($input) {
+  global $pairs;
   $removed = 0;
   do {
-    $removed = react($input);
+    $input = str_replace($pairs, '', $input, $removed);
   } while ($removed != 0);
   return $input;
 }
 
-echo 'Part 1: '.strlen(fullyReact($file))."\n";
+echo 'Part 1: '.strlen(react($file))."\n";;
 
 $lengths = array();
 for($i = 65; $i < 91; $i++) {
   $input = $file;
-  $test = str_replace(chr($i), '', $input);
-  $test = str_replace(chr($i+32), '', $test);
-  $lengths[chr($i)] = strlen(fullyReact($test));
+  $test = str_replace([chr($i),chr($i+32)], '', $input);
+  $lengths[chr($i)] = strlen(react($test));
 }
 echo 'Part 2: '.min($lengths);
