d058a6cf3baadfc20b07119b1a13a5b020480982
Greg Holmes <git@greg.holmes.name>
2018-12-06 23:08:12 +0000

    Change code for Chris.

--- 1/src/main/kotlin/main.kt
+++ 1/src/main/kotlin/main.kt
@@ -4,20 +4,18 @@ import kotlin.streams.toList
 import kotlin.system.measureTimeMillis
 
 fun main(args: Array<String>) {
-    val num = run {
-        val lineList = File("input.txt").useLines { lines -> lines.map { s -> s.toInt() }.asStream().toList() }
-        println("Part 1: ${lineList.sum()}")
-        val seen = mutableSetOf<Int>()
-        var sum = 0
-        while (true) {
-            lineList.forEach { line ->
-                    sum += line
-                    if (sum in seen) {
-                        return@run sum
-                    }
-                    seen.add(sum)
-            }
-        }
+    val lineList = File("input.txt").useLines { lines ->
+        lines.map { s -> s.toInt() }.asStream().toList()
     }
-    println("Part 2: $num")
+    println("Part 1: ${lineList.sum()}")
+    val seen = mutableSetOf<Int>()
+    var sum = 0
+    generateSequence { lineList }.flatten().forEach {
+        sum += it
+        if (sum in seen) {
+            println("Part 2: $sum")
+            return
+        }
+        seen.add(sum)
+    }
 }
