2d22e31b719f550d5a8ab3b843f9989a696698a6
Greg Holmes <git@greg.holmes.name>
2018-12-06 23:11:50 +0000

    Hide all the gradle.

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,7 +16,7 @@ if [ $# -eq 1 ]; then
   else
     if [ -f $1/build.gradle ]; then
       cd $1
-      gradle --project-cache-dir=/tmp/.gradle -g /tmp/.gradle --no-daemon jar
+      gradle --project-cache-dir=/tmp/.gradle -g /tmp/.gradle --no-daemon jar -q > /dev/null 2>&1
       time java -jar /tmp/build/libs/app.jar
     else
       time php $1/run.php
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-IMAGE=greboid/aoc-2018-09
+IMAGE=greboid/aoc-2018-10
 if [ "$(docker images -q $IMAGE)" == "" ]; then
     docker build . -t $IMAGE
 fi
