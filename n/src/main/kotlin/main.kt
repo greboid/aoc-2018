b3991eaf1cba85640fdba45e4e845bcaa964495a
Greg Holmes <git@greg.holmes.name>
2018-12-08 15:06:49 +0000

    Time template day.

--- n/src/main/kotlin/main.kt
+++ n/src/main/kotlin/main.kt
@@ -1,5 +1,8 @@
 fun main() {
-    val answer = 0
-    println("Part 1: ${answer}")
-    println("Part 2: ${answer}")
+    val ms = measureTimeMillis {
+        val answer 0
+        println("Part 1: ${answer}")
+        println("Part 2: ${answer}")
+    }
+    println("${TimeUnit.MILLISECONDS.toMinutes(ms)}m${TimeUnit.MILLISECONDS.toSeconds(ms)}.${TimeUnit.MILLISECONDS.toMillis(ms).toString().padStart(3, '0')}s")
 }
