4e22c3dcf56ad5e5e359da642ac79cd0c67f8058
Greg Holmes <git@greg.holmes.name>
2018-12-09 22:19:50 +0000

    Rust file.

---
+++ 1/src/blargh.rs
@@ -0,0 +1,42 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn main() -> io::Result<()> {
+    let f = File::open("/app/1/input.txt")?;
+    let reader = BufReader::new(f);
+    let numbers: Vec<i32> = reader
+        .lines()
+        .map(|e| e.unwrap().parse::<i32>().unwrap())
+        .collect();
+    part_one(&numbers)?;
+    part_two(&numbers)?;
+    Ok(())
+}
+
+fn part_one(input: &Vec<i32>) -> io::Result<()> {
+    let mut sum = 0;
+    for i in (0..input.len()) {
+      sum += input[i];
+    }
+    println!("Part 1: {}", sum);
+    Ok(())
+}
+
+fn part_two(input: &Vec<i32>) -> io::Result<()> {
+    let mut seen = HashSet::<i32>::new();
+    seen.insert(0);
+    let mut freq = 0;
+    for i in (0..input.len()).cycle() {
+        freq += input[i];
+        if seen.contains(&freq) {
+            break;
+        }
+        seen.insert(freq);
+    }
+
+    println!("Part 2: {}", freq);
+    Ok(())
+}
