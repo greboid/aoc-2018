424781e394335cc46cd79e9235e45214c908326f
Greg Holmes <git@greg.holmes.name>
2018-12-09 23:38:15 +0000

    Remove debug.

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,23 +1,18 @@
-use std::collections::HashSet; 
+use std::collections::HashMap;
 use std::fs::File; 
 use std::io; 
 use std::io::prelude::*; 
 use std::io::BufReader; 
-use std::time::Instant;
 
 fn main() -> io::Result<()> {
-    let start = Instant::now();
     let f = File::open("/app/1/input.txt")?;
     let reader = BufReader::new(f);
     let numbers: Vec<i32> = reader
         .lines()
         .map(|e| e.unwrap().parse::<i32>().unwrap())
         .collect();
-    println!("File: {:?}", start.elapsed() / 1000000);
     part_one(&numbers)?;
-    println!("Part 1: {:?}", start.elapsed() / 1000000);
     part_two(&numbers)?;
-    println!("Part 2: {:?}", start.elapsed() / 1000000);
     Ok(())
 }
 
@@ -31,14 +26,14 @@ fn part_one(input: &Vec<i32>) -> io::Result<()> {
 }
 
 fn part_two(input: &Vec<i32>) -> io::Result<()> {
-    let mut seen = HashSet::with_capacity(2^18);
+    let mut seen = HashMap::with_capacity(2^18);
     let mut freq = 0;
     for i in (0..input.len()).cycle() {
         freq += input[i];
-        if seen.contains(&freq) {
+        if seen.contains_key(&freq) {
             break;
         }
-        seen.insert(freq);
+        seen.insert(freq, 1);
     }
     println!("Part 2: {}", freq);
     Ok(())
