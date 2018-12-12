d3bb85fa7468054b2cb31b0c1531c78fc49238c0
Greg Holmes <git@greg.holmes.name>
2018-12-12 19:47:47 +0000

    USe rust image for rust days.

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
-#!/bin/bash
-IMAGE=greboid/aoc-2018-15
+if [ $# -ge 1 ]; then
+  if [ ! -d "$1" ]; then
+    echo "Invalid Day"
+    exit 1
+  fi
+fi
+if [ -f $1/src/main.rs ]; then
+  IMAGE=greboid/aoc-2018-rust-01
+  FILE=Dockerfile-rust
+else
+  IMAGE=greboid/aoc-2018-15
+  FILE=Dockerfile
+fi
 if [ "$(docker images -q $IMAGE)" == "" ]; then
-    docker build . -t $IMAGE
+  docker build -f$FILE . -t $IMAGE
 fi
 docker run --rm -it -v $(pwd):/app $IMAGE $@
