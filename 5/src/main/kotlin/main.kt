31071c13aaa892833740e9cdfc5b591cc0972a0d
Greg Holmes <git@greg.holmes.name>
2018-12-14 19:11:08 +0000

    Optimise day 5.

--- 5/src/main/kotlin/main.kt
+++ 5/src/main/kotlin/main.kt
@@ -1,31 +1,32 @@
 import java.io.File
 import java.util.*
+import kotlin.system.measureTimeMillis
 
 fun main() {
-    val input = File("input.txt").readLines().first()
-    println("Part 1\t: ${react(input).length}")
-    println("Part 2\t: ${fullyReact(input).length}")
+    val ms = measureTimeMillis {
+        val input = File("input.txt").readLines().first()
+        println("Part 1\t: ${react(input, null)}")
+        println("Part 2\t: ${fullyReact(input)}")
+    }
+    println(ms)
 }
 
-fun react(input: String): String {
-    with(Stack<Char>()) {
-        input.toCharArray().forEach {
-            if (!empty() && peek().toInt().xor(it.toInt()) == 32) {
-                pop()
+fun react(input: String, skip: Char?): Int {
+    val stack = Stack<Char>()
+    input.toCharArray().forEach {
+        if (skip != null && (skip == it || skip.toInt().xor(it.toInt()) == 32)) {
+            //Skip
+        } else {
+            if (!stack.empty() && stack.peek().toInt().xor(it.toInt()) == 32) {
+                stack.pop()
             } else {
-                push(it)
+                stack.push(it)
             }
         }
-        return joinToString("")
     }
+    return stack.size
 }
 
-fun fullyReact(input: String): String {
-    return ('a' until 'z').map {
-        input.replace(it.toString(), "", true)
-    }.map {
-        react(it)
-    }.minBy {
-        it.length
-    } ?: ""
+fun fullyReact(input: String): Int {
+    return ('a' until 'z').asSequence().map { react(input, it) }.min()!!
 }
