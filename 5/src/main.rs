a3bb3646e6acc25e602ed633a96f4c6e65fbe1b1
Greg Holmes <git@greg.holmes.name>
2018-12-15 13:32:14 +0000

    Optimise day 5

--- 5/src/main.rs
+++ 5/src/main.rs
@@ -6,11 +6,12 @@ fn main() {
     let mut input = String::new();
     file.read_to_string(&mut input).expect("Unable to read file");
     input = input.trim().to_string();
-    println!("Part 1: {}", react(&input, None));
-    println!("Part 2: {}", fully_react(&input));
+    let part1 = react(&input, None);
+    println!("Part 1: {}", part1.len());
+    println!("Part 2: {}", fully_react(&part1));
 }
 
-fn react(input: &str, skip: Option<char>) -> usize {
+fn react(input: &str, skip: Option<char>) -> String {
   let mut stack = Vec::<char>::new();
   for c in input.chars() {
     if skip.is_some() && (skip.unwrap() == c || reacts(c, skip.unwrap())) {
@@ -23,13 +24,14 @@ fn react(input: &str, skip: Option<char>) -> usize {
       }
     }
   }
-  return stack.len()
+  return stack.into_iter().collect()
 }
 
 fn fully_react(input: &str) -> usize {
-  return (b'a'..=b'z').map(|c| react(&input, Some(c as char))).min().unwrap()
+  return (b'a'..=b'z').map(|c| react(&input, Some(c as char)).len()).min().unwrap()
 }
 
 fn reacts(a: char, b: char) -> bool {
     return a != b && a.eq_ignore_ascii_case(&b);
 }
+
