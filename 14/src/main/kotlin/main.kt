825f0279f5f65b163893677f9dbd52eb2f8e327c
Greg Holmes <git@greg.holmes.name>
2018-12-18 21:35:42 +0000

    Sprinkle some Kotlin voodoo for optimisation.

--- 14/src/main/kotlin/main.kt
+++ 14/src/main/kotlin/main.kt
@@ -4,40 +4,23 @@ import kotlin.system.measureTimeMillis
 fun main() {
     val ms = measureTimeMillis {
         val input = File("input.txt").readLines().first()
-        println("Part 1: ${part1(input.toInt())}")
-        println("Part 2: ${part2(input)}")
+        println("Part 1: ${scores.drop(input.toInt()).take(10).joinToString("")}")
+        println("Part 2: ${scores.windowed(input.length).indexOf(input.toCharArray().map(Character::getNumericValue))}")
     }
     println(ms)
 }
 
-fun part1(input: Int): String {
+private val scores = sequence {
+    val score = arrayOf(0, 1)
     val recipes = mutableListOf(3, 7)
-    val scores = arrayOf(0, 1)
-
-    while (recipes.size < input + 10) {
-        step(recipes, scores)
+    yieldAll(recipes)
+    while (true) {
+        val digits = (recipes[score.first()] + recipes[score.second()]).toString().map(Character::getNumericValue)
+        recipes.addAll(digits)
+        yieldAll(digits)
+        score[0] = (score.first() + recipes[score.first()] + 1) % recipes.size
+        score[1] = (score.second() + recipes[score.second()] + 1) % recipes.size
     }
-    return recipes.drop(input).take(10).joinToString("")
-}
-
-fun part2(input: String): Int {
-    val recipes = mutableListOf(3, 7)
-    val scores = arrayOf(0, 1)
-
-    while (checkPart2End(input, recipes)) {
-        step(recipes, scores)
-    }
-    return recipes.joinToString("").indexOf(input)
-}
-
-fun step(recipes: MutableList<Int>, score: Array<Int>){
-    recipes += (recipes[score.first()] + recipes[score.second()]).toString().map(Character::getNumericValue)
-    score[0] = (score.first() + recipes[score.first()] + 1) % recipes.size
-    score[1] = (score.second() + recipes[score.second()] + 1) % recipes.size
-}
-
-private fun checkPart2End(input: String, recipes: MutableList<Int>): Boolean {
-    return input !in recipes.takeLast(11).joinToString("")
 }
 
 private fun Array<Int>.second() = this[1]
