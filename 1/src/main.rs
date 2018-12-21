6ff40927de23c59ecb0c779652c3224bc0079174
Greg Holmes <git@greg.holmes.name>
2018-12-21 23:56:13 +0000

    Remove debug timing on day 1.

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -1,14 +1,11 @@
 extern crate hashbrown;
-extern crate time;
 
 use hashbrown::HashSet;
 use std::fs::File; 
 use std::io::prelude::*;
 use std::io::BufReader;
-use time::PreciseTime;
 
 fn main() {
-    let start = PreciseTime::now();
     let f = File::open("input.txt").expect("File not found");
     let reader = BufReader::new(f);
     let numbers: Vec<i32> = reader
@@ -16,9 +13,7 @@ fn main() {
         .map(|e| e.unwrap().parse::<i32>().unwrap())
         .collect();
     println!("Part 1: {}", part_one(&numbers));
-    println!("{}.", start.to(PreciseTime::now()));
     println!("Part 2: {}", part_two(&numbers));
-    println!("{}.", start.to(PreciseTime::now()));
 }
 
 fn part_one(input: &Vec<i32>) -> i32 {
