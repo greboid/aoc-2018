859b314163d7c8a0df64b3c93e9d7ade8075080d
Greg Holmes <git@greg.holmes.name>
2018-12-21 22:28:37 +0000

    Maybe optimise day 1?

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -2,38 +2,36 @@ extern crate hashbrown;
 
 use hashbrown::HashSet;
 use std::fs::File; 
-use std::io; 
-use std::io::prelude::*; 
+use std::io::prelude::*;
 use std::io::BufReader; 
 
-fn main() -> io::Result<()> {
-    let f = File::open("/app/1/input.txt")?;
+fn main() {
+    let f = File::open("input.txt").expect("File not found");
     let reader = BufReader::new(f);
     let numbers: Vec<i32> = reader
         .lines()
         .map(|e| e.unwrap().parse::<i32>().unwrap())
         .collect();
-    part_one(&numbers)?;
-    part_two(&numbers)?;
-    Ok(())
+    part_one(&numbers);
+    part_two(&numbers);
 }
 
-fn part_one(input: &Vec<i32>) -> io::Result<()> {
+fn part_one(input: &Vec<i32>) {
     println!("Part 1: {}", input.iter().sum::<i32>());
-    Ok(())
 }
 
-fn part_two(input: &Vec<i32>) -> io::Result<()> {
+fn part_two(input: &Vec<i32>) -> i32 {
     let mut seen = HashSet::new();
-    let frequency = input
-        .iter()
-        .cycle()
-        .scan(0, |frequency, &change| {
-            *frequency += change;
-            Some(*frequency)
-        })
-        .find(|frequency| !seen.insert(*frequency))
-        .unwrap();
-    println!("Part 2: {}", frequency);
-    Ok(())
+    let mut sum = 0;
+    loop {
+        for n in input {
+            sum += n;
+            if seen.contains(&sum) {
+                println!("Part 1: {}", sum);
+                return sum;
+            }
+            seen.insert(sum);
+        }
+    }
 }
+
