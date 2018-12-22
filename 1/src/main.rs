22aeb8598fe67a0c8645449ae06247f2ae141d16
Greg Holmes <git@greg.holmes.name>
2018-12-22 00:31:56 +0000

    Maybe optimise?

--- 1/src/main.rs
+++ 1/src/main.rs
@@ -5,36 +5,31 @@ use hashbrown::HashSet;
 use std::fs::File; 
 use std::io::prelude::*;
 use std::io::BufReader;
+use std::io::SeekFrom;
 
 fn main() {
     let f = File::open("input.txt").expect("File not found");
-    let reader = BufReader::new(f);
+    let mut reader = BufReader::new(f);
     let mut sum = 0;
-    let mut temp = 0;
-    let numbers: Vec<i32> = reader
-        .lines()
-        .map(|e| {
-            temp = e.unwrap().parse::<i32>().unwrap();
-            sum += temp;
-            return temp;
-        })
-        .collect();
-    println!("Part 1: {}", sum);
-    println!("Part 2: {}", part_two(&numbers));
-}
-
-fn part_two(input: &Vec<i32>) -> i32 {
-    let mut seen = HashSet::with_capacity(200000);
-    let mut sum = 0;
+    let mut temp;
+    let mut iteration = 0;
     let mut check;
+    let mut seen = HashSet::with_capacity(200000);
     loop {
-        for n in input {
-            sum += n;
+        for line in reader.by_ref().lines() {
+            temp = line.unwrap().parse::<i32>().unwrap();
+            sum += temp;
             check = seen.replace(sum);
             if check != None {
-                return check.unwrap()
+                println!("Part 2: {}", check.unwrap());
+                return
             }
         }
+        if iteration == 0 {
+            println!("Part 1: {}", sum);
+        }
+        reader.seek(SeekFrom::Start(0)).expect("Can't seek");
+        iteration += 1;
     }
 }
 
